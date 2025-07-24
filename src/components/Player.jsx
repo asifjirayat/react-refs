e41177c7fd1e2806540f02f606bbf1e3e4fd45d9
Asif <asifjirayat@gmail.com>
2025-07-24 18:46:52 +0530

    Created Player component

---
+++ src/components/Player.jsx
@@ -0,0 +1,11 @@
+export default function Player() {
+  return (
+    <section id="player">
+      <h2>Welcome Unknown Entity</h2>
+      <p>
+        <input type="text"></input>
+        <button>Set Name</button>
+      </p>
+    </section>
+  );
+}
